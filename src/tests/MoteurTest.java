8b0dfd26176c9be35aef5f3217acbab2cdc67103
modeprobe <adrien.gareau@gmail.com>
2013-10-18 23:42:06 +0200

    ajout test moteur

---
+++ src/tests/MoteurTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import action.Moteur;
+
+public class MoteurTest {
+
+	public static void main(String[] args) {
+		
+		Moteur mot = new Moteur();
+		mot.avancer(2);
+		
+		
+	}
+}
