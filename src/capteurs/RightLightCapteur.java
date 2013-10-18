4efc47a6c4db6a291d21789dcad45bd9c6eead91
gabriel-farache <gabriel.farache@gmail.com>
2013-10-19 00:00:44 +0200

    Activate captors before test

--- src/capteurs/RightLightCapteur.java
+++ src/capteurs/RightLightCapteur.java
@@ -8,6 +8,7 @@ public class RightLightCapteur implements ICapteursFonctions{
 	LightSensor light = new LightSensor(SensorPort.S3);
 	@Override
 	public int executerDetection() {
+		SensorPort.S3.activate();
 		return SensorPort.S3.readValue();
 	}
 
