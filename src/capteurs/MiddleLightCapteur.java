8aa0fcc6c1d5e8fa99a85fdf2a87def9d093d42a
gabriel-farache <gabriel.farache@gmail.com>
2013-10-18 23:42:34 +0200

    Modif tests  + capteurs

--- src/capteurs/MiddleLightCapteur.java
+++ src/capteurs/MiddleLightCapteur.java
@@ -1,9 +1,11 @@
 package capteurs;
 
+import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 
 public class MiddleLightCapteur implements ICapteursFonctions {
 
+	LightSensor light = new LightSensor(SensorPort.S2);
 	@Override
 	public int executerDetection() {
 		return SensorPort.S2.readValue();
