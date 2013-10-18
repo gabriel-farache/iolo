98935ec8ec3a11f27fafb0edd403cd6e8ac7a99e
mmosiejan <m.mosiejanczyk@srmvision.com>
2013-10-19 01:06:38 +0200

    controller ligne droite

--- src/controller/CapteurController.java
+++ src/controller/CapteurController.java
@@ -1,46 +1,58 @@
 package controller;
 
 import action.Moteur;
-import capteurs.* ;
+import capteurs.*;
 
 public class CapteurController {
-	public static final int CORRECTION_ANGLE = 7 ;
-	int rc ;
-	int mc ;
-	int uc ;
-	int cc ;
-	Thread trc ;
-	Thread tmc ;
-	Thread tuc ;
-	Thread tcc ;
-	Moteur moteur ;
-	
-	public void init(){
-		trc = new Thread(new Capteur(new RightLightCapteur(),this));		
-		tmc = new Thread(new Capteur(new MiddleLightCapteur(),this));
-		tuc = new Thread(new Capteur(new UltrasonCapteur(),this));
-		tcc = new Thread(new Capteur(new ColorCapteur(),this));
+	public static final int CORRECTION_ANGLE = 7;
+	int rc;
+	int mc;
+	int uc;
+	int cc;
+	Thread trc;
+	Thread tmc;
+	Thread tuc;
+	Thread tcc;
+	Moteur moteur;
+	boolean estEnVirage = false ;
+
+	public void init() {
+		trc = new Thread(new Capteur(new RightLightCapteur(), this));
+		tmc = new Thread(new Capteur(new MiddleLightCapteur(), this));
+		tuc = new Thread(new Capteur(new UltrasonCapteur(), this));
+		tcc = new Thread(new Capteur(new ColorCapteur(), this));
 		moteur = new Moteur();
 	}
 
-	public void start(){
+	public void start() {
 		trc.start();
 		tmc.start();
 		tuc.start();
 		tcc.start();
 	}
-	
+
 	public void setRc(int rc) {
 		this.rc = rc;
-		if (Math.abs(rc - ICapteursFonctions.RIGHT_LIGHT_NOIR)>ICapteursFonctions.OFFSET){
+		if (Math.abs(rc - ICapteursFonctions.RIGHT_LIGHT_GRIS) < ICapteursFonctions.OFFSET) {
+			estEnVirage = true ;
+			moteur.ralentir(1f);
+			virageImminent();
+		} else if (Math.abs(rc - ICapteursFonctions.RIGHT_LIGHT_BLANC) < ICapteursFonctions.OFFSET) {
+			moteur.turn(CORRECTION_ANGLE);
+		} else if ((Math.abs(rc - ICapteursFonctions.RIGHT_LIGHT_NOIR) > ICapteursFonctions.OFFSET) && estEnVirage ) {
+			estEnVirage = false ;
+			moteur.accelerer(2);
+		} else if (Math.abs(rc - ICapteursFonctions.RIGHT_LIGHT_NOIR) < ICapteursFonctions.OFFSET){
 			moteur.turn(-CORRECTION_ANGLE);
 		}
 	}
 
 	public void setMc(int mc) {
 		this.mc = mc;
-		if (Math.abs(mc - ICapteursFonctions.MIDDLE_LIGHT_NOIR)>ICapteursFonctions.OFFSET){
-			moteur.turn(CORRECTION_ANGLE);
+		if (Math.abs(mc - ICapteursFonctions.MIDDLE_LIGHT_NOIR) < ICapteursFonctions.OFFSET) {
+			moteur.turn(0);
+		} else {
+			moteur.turn(-CORRECTION_ANGLE);
 		}
 	}
 
@@ -50,9 +62,9 @@ public class CapteurController {
 
 	public void setCc(int cc) {
 		this.cc = cc;
-		switch(cc){
+		switch (cc) {
 		case ICapteursFonctions.COLOR_BLEU:
-			break ;
+			break;
 		case ICapteursFonctions.COLOR_JAUNE:
 			break;
 		case ICapteursFonctions.COLOR_NOIR:
@@ -60,8 +72,21 @@ public class CapteurController {
 		case ICapteursFonctions.COLOR_ROUGE:
 			break;
 		case ICapteursFonctions.COLOR_VERT:
-			break ;
+			break;
 		case ICapteursFonctions.COLOR_WHITE:
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void virageImminent() {
+		switch (cc) {
+		case ICapteursFonctions.COLOR_NOIR:
+			break;
+		case ICapteursFonctions.COLOR_ROUGE:
+			break;
+		case ICapteursFonctions.COLOR_VERT:
 			break;
 		default:
 			break;
--- src/controller/Main.java
+++ src/controller/Main.java
@@ -9,16 +9,8 @@ import action.affichages.Son;
 public class Main {
 
 	public static void main(String[] args) {
-		Son son = new Son();
-		for (int i=0;i<11;i++){
-			son.playLego(i);
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		CapteurController controller = new CapteurController();
+		controller.init();
 	}
 
 }
